cbb1db618753db32e7cb11db053eb1cee92bbc14
YuraPuzyrev <refresh.93@mail.ru>
2018-08-29 20:52:49 +0300

    add new changes

---
+++ src/org/itstep/runner/Cycles.java
@@ -0,0 +1,66 @@
+package org.itstep.runner;
+import java.util.Scanner;
+public class Cycles {
+    public static void main(String[] args){
+        //ЗАДАНИЕ №1
+        /*for (int i = 1; i<100; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }*/
+
+        //ЗАДАНИЕ №2
+        /*Scanner p = new Scanner(System.in);
+        System.out.println("Введите значение факториала");
+        int f = p.nextInt();
+        int j = 1;
+        for(int k = 1; k<=f; k++){
+            j=j*k;
+        }
+        System.out.println(j);*/
+
+        //ЗАДАНИЕ №3
+        /*int i = 1;
+        while(i<100){
+            if(i%2 != 0){
+                System.out.print(i + " ");
+            }
+            i++;
+        }*/
+
+        /*Scanner p = new Scanner(System.in);
+        System.out.println("Введите значение факториала");
+        int f = p.nextInt();
+        int j = 1;
+        int k = 1;
+        while(k<=f){
+            j=j*k;
+            k++;
+        }
+        System.out.println(j);*/
+
+        //ЗАДАНИЕ №4
+        /*Scanner p = new Scanner(System.in);
+        System.out.println("Введите значение пременной");
+        int x = p.nextInt();
+        System.out.println("Введите значение степени");
+        int n = p.nextInt();
+        int y = 1;
+        for(int k = 1; k<=n; k++){
+            y=y*x;
+        }
+        System.out.println(y);*/
+
+        //ЗАДАНИЕ №5
+        /*Scanner p = new Scanner(System.in);
+        System.out.println("Введите значение факториала");
+        int f = p.nextInt();
+        int j = 1;
+        int k = 1;
+        do {
+            j=j*k;
+            k++;
+        } while(k<=f);
+        System.out.println(j);*/
+    }
+}
