440cd590158a62f6891e0a21cb599d12d1115487
YuraPuzyrev <refresh.93@mail.ru>
2018-09-01 02:45:22 +0300

    new home task done

--- src/org/itstep/runner/Cycles.java
+++ src/org/itstep/runner/Cycles.java
@@ -1,7 +1,8 @@
 package org.itstep.runner;
 import java.util.Scanner;
+import java.lang.String;
 public class Cycles {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //ЗАДАНИЕ №1
         /*for (int i = 1; i<100; i++) {
             if (i % 2 != 0) {
@@ -62,5 +63,39 @@ public class Cycles {
             k++;
         } while(k<=f);
         System.out.println(j);*/
+
+        //ЗАДАНИЕ №6*
+        /*Scanner a = new Scanner(System.in);
+        int x;
+        int y;
+        int z;
+        do {
+            System.out.println("Введите первое целое число");
+            while (!a.hasNextInt()){
+                System.out.println("Введите целое число!");
+                a.nextLine();
+            }
+            x = a.nextInt();
+        } while (x <= 0);
+        System.out.println("Вы ввели число: " +x);
+        do {
+            System.out.println("Введите второе целое число");
+            while (!a.hasNextInt()){
+                System.out.println("Введите целое число!");
+                a.nextLine();
+            }
+            y = a.nextInt();
+        } while (y <= 0);
+        System.out.println("Вы ввели число: " +y);
+        do {
+            System.out.println("Введите третье целое число");
+            while (!a.hasNextInt()){
+                System.out.println("Введите целое число!");
+                a.nextLine();
+            }
+            z = a.nextInt();
+        } while (z <= 0);
+        System.out.println("Вы ввели число: " +z);
+        System.out.println("Вы ввели числа: " +x+ " " +y+ " " +z);*/
     }
 }
