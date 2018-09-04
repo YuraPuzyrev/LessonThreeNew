2f827fd159c0bcf20d776347c3b64e4b5ae53a2e
qa <sjdvb@rkerg.ru>
2018-09-04 18:47:04 +0300

    taskfourdone

--- src/org/itstep/runner/TaskFour.java
+++ src/org/itstep/runner/TaskFour.java
@@ -5,19 +5,29 @@ public class TaskFour {
     public static void main(String[] args) {
         Random r = new Random();
         int a = r.nextInt(20);
+        int t = 0;
         System.out.println("Угадайте число! У вас есть ровно пять попыток!");
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите число от 0 до 20:");
-        int b = sc.nextInt();
         do {
+            int b = sc.nextInt();
             if (b<a) {
-                System.out.println("Мало! Попробуйте ещё разок:");
+                t++;
+                if (t<5) {
+                System.out.println("Мало! Попробуйте ещё разок:");}
+                sc.nextLine();
             } else if (b>a) {
+                t++;
+                if (t<5) {
                 System.out.println("Много! Попробуйте ещё разок:");}
-            while (a == b) {
-                System.out.println("Победа!");
-                a++;
+                sc.nextLine();
             }
-        } while (a<5);
+            else if (a == b) {
+                System.out.println("Победа!");
+                break;
+            } if (t>=5) {
+                System.out.println("Поражение!");
+            }
+        } while (t<5);
     }
 }
