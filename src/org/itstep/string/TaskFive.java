6b0a2839d26f19178f75a2bb6e22a61e4a7805d8
qa <sjdvb@rkerg.ru>
2018-09-11 19:27:06 +0300

    razbor domashki

--- src/org/itstep/string/TaskFive.java
+++ src/org/itstep/string/TaskFive.java
@@ -1,9 +1,30 @@
 package org.itstep.string;
 
+import java.util.Scanner;
+
 public class TaskFive {
     public static void main(String[] args) {
-        Character a = new Character('T');
+        /*Character a = new Character('T');
         Character b = Character.toLowerCase(a);
-        System.out.println(b);
+        System.out.println(b);*/
+
+        //В классе:
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите один латинский символ");
+        String tempValue = scanner.nextLine();
+        if(tempValue.length()==1){
+            char ch = tempValue.charAt(0);
+            if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z')){
+                if(Character.isLowerCase(ch)){
+                    System.out.println("Результат = " +Character.toUpperCase(ch));
+                }else {
+                    System.out.println("Результат = " +Character.toLowerCase(ch));
+                }
+            }else {
+                System.out.println("Символ не латинский");
+            }
+        }else {
+            System.out.println("Некорректный ввод");
+        }
     }
 }
--- src/org/itstep/string/TaskFour.java
+++ src/org/itstep/string/TaskFour.java
@@ -1,10 +1,12 @@
 package org.itstep.string;
 
+import java.util.Arrays;
+
 public class TaskFour {
     public static void main(String[] args) {
-        String a = new String("Сегодня самый лучший день в году.");
+        String a = new String("Сегодня   самый лучший день в году. ");
         //Способ №1
-        int sum = 0;
+        /*int sum = 0;
         Character g = new Character(' ');
         System.out.println(g.isWhitespace(g));
         for (int c = 0; c<a.length();c++){
@@ -12,10 +14,28 @@ public class TaskFour {
             if(x==g){
                 sum++;}
         }
-        System.out.println("В строке " +(sum+1)+ " слов");
+        System.out.println("В строке " +(sum+1)+ " слов");*/
 
         //Способ №2
         /*int b = a.split(" ").length;
         System.out.println("В строке " +b+ " слов");*/
+
+        //Способ в классе
+        /*String[] result = a.split(" ");
+        System.out.println(Arrays.toString(result));
+        int counter = 0;
+        for (int i = 0; i < result.length; i++) {
+            if (!result[i].isEmpty()) {
+                counter++;
+            }
+        }
+        /*for (String x : result) {
+            if (!x.isEmpty()) {
+                counter++;
+            }*/
+        //Способ через регулярное выражение "+"(одно или более повторений)
+        String[] result = a.split(" +");
+        System.out.println(Arrays.toString(result));
+        System.out.println("Количество слов равно " + result.length);
+        }
     }
-}
